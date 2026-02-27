9da01c7e20f366669ee9935f9c0fccd7dd0465a8
SadLibs Dev <dev@sadlibs.online>
2026-02-26 21:43:41 -0500

    UI Tweaks: Remove global index.css from V2 production build to prevent cascading layout leakage; center center background zoom.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import './alt.css'
 import { Analytics } from '@vercel/analytics/react'
 
